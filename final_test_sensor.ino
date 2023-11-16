8759b3dd7f2885e0215008ceec27422b2869bdbd
OMAKASE <atichat645@gmail.com>
2023-11-16 15:37:55 +0700

    Update final_test_sensor.ino

--- final_test_sensor.ino
+++ final_test_sensor.ino
@@ -1,11 +1,58 @@
+#include <WiFi.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
-#include "DHT.h"
+#include <DHT.h>
 #include <BH1750.h>
 #include <Adafruit_SI1145.h>
+extern "C" {
+  #include "freertos/FreeRTOS.h"
+  #include "freertos/timers.h"
+}
+#include <AsyncMqttClient.h>
 
-// Pin configurations for different sensors
+#define WIFI_SSID "IP-0349" //OMAKASE_2.4G
+#define WIFI_PASSWORD "yoyoyoyo" //0936540643
+
+// Digital Ocean MQTT Mosquitto Broker
+#define MQTT_HOST IPAddress(139, 59, 243, 38)
+// For a cloud MQTT broker, type the domain name
+//#define MQTT_HOST "example.com"
+#define MQTT_PORT 1883
+
+#define MQTT_USERNAME "ESP01"
+#define MQTT_PASSWORD "zx123456"
+
+// Test MQTT Topic
+#define MQTT_SUB_TEST "test"
+#define MQTT_PUB_TEST "test"
+
+//MQTT Topic
+//PUB
+#define MQTT_PUB_MOI "SoilMoisture"
+#define MQTT_PUB_HUM "Humidity"
+#define MQTT_PUB_TEMP "Temperature"
+#define MQTT_PUB_LR "LightLevel"
+#define MQTT_PUB_UV "UVIndex"
+
+//control Topic
+#define MQTT_PUB_L1 "Light1"
+#define MQTT_PUB_L2 "Light2"
+#define MQTT_PUB_L3 "Light3"
+#define MQTT_PUB_L4 "Light4"
+#define MQTT_PUB_P1 "Pump1"
+#define MQTT_PUB_P2 "Pump2"
+#define MQTT_PUB_S1 "Servo1"
+
+//SUB
+#define MQTT_SUB_L1 "Light1"
+#define MQTT_SUB_L2 "Light2"
+#define MQTT_SUB_L3 "Light3"
+#define MQTT_SUB_L4 "Light4"
+#define MQTT_SUB_P1 "Pump1"
+#define MQTT_SUB_P2 "Pump2"
+#define MQTT_SUB_S1 "Servo1"
+
 #define DHTPIN 13     // Digital pin connected to the DHT sensor
 #define BH1750_SDA 26  // SDA pin for BH1750 sensor
 #define BH1750_SCL 27  // SCL pin for BH1750 sensor
@@ -17,66 +64,296 @@
 // Uncomment whatever type you're using!
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 
+// Pin connected to the Lihght1
+#define L1Pin 12
+#define L2Pin 15
+#define L3Pin 16
+#define L4Pin 17
+#define P1Pin 21
+// Pin connected to the Fan
+#define P2Pin 2
+#define S1Pin 0
+
 DHT dht(DHTPIN, DHTTYPE);
 BH1750 lightMeter;
 Adafruit_SI1145 uv;
 
-int _moisture, sensor_analog;
+//init Global variable
+float h=0.00,t=0.00,lux=0.00;
+int _moisture=0, val=0;
+
+int sensor_analog;
 #define sensor_pin SOIL_MOISTURE_PIN  /* Soil moisture sensor O/P pin */
 
 #define SEALEVELPRESSURE_HPA (1013.25)  // Define SEALEVELPRESSURE_HPA globally
 
-void setup() {
-  Serial.begin(9600);
+AsyncMqttClient mqttClient;
+TimerHandle_t mqttReconnectTimer;
+TimerHandle_t wifiReconnectTimer;
 
-  // Initialize the sensors
-  dht.begin();
-  lightMeter.begin(BH1750::ONE_TIME_HIGH_RES_MODE);  // BH1750 sensor in high-resolution mode
-  uv.begin();
+unsigned long previousMillis = 0;   // Stores last time temperature was published
+const long interval = 5000;         // Interval at which to publish sensor readings
+
+int i = 0;
+
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}
+
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+void WiFiEvent(WiFiEvent_t event) {
+  Serial.printf("[WiFi-event] event: %d\n", event);
+  switch(event) {
+    case SYSTEM_EVENT_STA_GOT_IP:
+      Serial.println("WiFi connected");
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+      connectToMqtt();
+      break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+      Serial.println("WiFi lost connection");
+      xTimerStop(mqttReconnectTimer, 0); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+      xTimerStart(wifiReconnectTimer, 0);
+      break;
+  }
+}
+
+// Add more topics that want your ESP to be subscribed to
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+  
+  // ESP subscribed to test topic
+  //uint16_t packetIdSub = mqttClient.subscribe(MQTT_SUB_TEST, 0);
+  int16_t packetIdSub_L1 = mqttClient.subscribe(MQTT_SUB_L1, 0);
+  int16_t packetIdSub_L2 = mqttClient.subscribe(MQTT_SUB_L2, 0);
+  int16_t packetIdSub_L3 = mqttClient.subscribe(MQTT_SUB_L3, 0);
+  int16_t packetIdSub_L4 = mqttClient.subscribe(MQTT_SUB_L4, 0);
+  int16_t packetIdSub_P1 = mqttClient.subscribe(MQTT_SUB_P1, 0);
+  int16_t packetIdSub_P2 = mqttClient.subscribe(MQTT_SUB_P2, 0);
+  int16_t packetIdSub_S1 = mqttClient.subscribe(MQTT_SUB_S1, 0);
+  Serial.println("Subscribing at QoS 0");
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+  if (WiFi.isConnected()) {
+    xTimerStart(mqttReconnectTimer, 0);
+  }
+}
+
+void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+  Serial.println("Subscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.print("  qos: ");
+  Serial.println(qos);
+}
+
+void onMqttUnsubscribe(uint16_t packetId) {
+  Serial.println("Unsubscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+// You can modify this function to handle what happens when you receive a certain message in a specific topic
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  String messageTemp;
+  for (int i = 0; i < len; i++) {
+    //Serial.print((char)payload[i]);
+    messageTemp += (char)payload[i];
+  }
+  // Check if the MQTT message was received on topic
+  // if (strcmp(topic, MQTT_SUB_TEST) == 0) {
+  //   Serial.println("TRUE");
+  //  }
+  if (strcmp(topic, MQTT_SUB_L1) == 0) {
+     Serial.println("L1 Publish received.");
+     //Check and turn on/off Light1
+     if(strcmp(messageTemp.c_str(), "on") == 0){
+      digitalWrite(L1Pin, HIGH);
+      Serial.println("Status: L1 on");
+     }
+     else if(strcmp(messageTemp.c_str(), "off") == 0){
+      digitalWrite(L1Pin, LOW);
+      Serial.println("Status: L1 off");
+     }
+    }
+  if (strcmp(topic, MQTT_SUB_L2) == 0) {
+    Serial.println("L2 Publish received.");
+    //Check and turn on/off Light2
+    if(strcmp(messageTemp.c_str(), "on") == 0){
+     digitalWrite(L2Pin, HIGH);
+     Serial.println("Status: L2 on");
+    }
+    else if(strcmp(messageTemp.c_str(), "off") == 0){
+     digitalWrite(L2Pin, LOW);
+     Serial.println("Status: L2 off");
+    }
+  }
+  if (strcmp(topic, MQTT_SUB_L3) == 0) {
+    Serial.println("Status: L3 Publish received.");
+    //Check and turn on/off Light4
+    if(strcmp(messageTemp.c_str(), "on") == 0){
+     digitalWrite(L3Pin, HIGH);
+     Serial.println("Status: L3 on");
+    }
+    else if(strcmp(messageTemp.c_str(), "off") == 0){
+     digitalWrite(L3Pin, LOW);
+     Serial.println("Status: L3 off");
+    }
+  }
+  if (strcmp(topic, MQTT_SUB_L4) == 0) {
+    Serial.println("L4 Publish received.");
+    //Check and turn on/off Light4
+    if(strcmp(messageTemp.c_str(), "on") == 0){
+     digitalWrite(L4Pin, HIGH);
+     Serial.println("Status: L4 on");
+    }
+    else if(strcmp(messageTemp.c_str(), "off") == 0){
+     digitalWrite(L1Pin, LOW);
+     Serial.println("Status: L4 off");
+    }
+  }
+  if (strcmp(topic, MQTT_SUB_P1) == 0) {
+    Serial.println("P1 Publish received.");
+    //Check and turn on/off Pump1
+    if(strcmp(messageTemp.c_str(), "on") == 0){
+     digitalWrite(P1Pin, HIGH);
+     Serial.println("Status: P1 on");
+    }
+    else if(strcmp(messageTemp.c_str(), "off") == 0){
+     digitalWrite(P1Pin, LOW);
+     Serial.println("Status: P1 off");
+    }
+  }
+  if (strcmp(topic, MQTT_SUB_P2) == 0) {
+    Serial.println("P2 Publish received.");
+    //Check and turn on/off Fan
+    if(strcmp(messageTemp.c_str(), "on") == 0){
+     digitalWrite(P2Pin, HIGH);
+     Serial.println("Status: Fan on");
+    }
+    else if(strcmp(messageTemp.c_str(), "off") == 0){
+     digitalWrite(P2Pin, LOW);
+     Serial.println("Status: Fan off");
+    }
+  }
+  if (strcmp(topic, MQTT_SUB_S1) == 0) {
+    Serial.println("S1 Publish received.");
+    //Check and turn on/off Servo
+    if(strcmp(messageTemp.c_str(), "on") == 0){
+     digitalWrite(S1Pin, HIGH);
+     Serial.println("Status: Servo1 on");
+    }
+    else if(strcmp(messageTemp.c_str(), "off") == 0){
+     digitalWrite(S1Pin, LOW);
+     Serial.println("Status: Servo1 off");
+    }
+  }
+ 
+  //Serial.println("Publish received.");
+  Serial.print("  message: ");
+  Serial.println(messageTemp);
+  Serial.print("  topic: ");
+  Serial.println(topic);
+//  Serial.print("  qos: ");
+//  Serial.println(properties.qos);
+//  Serial.print("  dup: ");
+//  Serial.println(properties.dup);
+//  Serial.print("  retain: ");
+//  Serial.println(properties.retain);
+//  Serial.print("  len: ");
+//  Serial.println(len);
+//  Serial.print("  index: ");
+//  Serial.println(index);
+//  Serial.print("  total: ");
+//  Serial.println(total);
+}
+
+void onMqttPublish(uint16_t packetId) {
+  Serial.print("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
 }
 
 void readDHT22() {
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-  if (isnan(h) || isnan(t)) {
-    Serial.println(F("Failed to read from DHT sensor!"));
-    return;
-  }
-  Serial.print(F("DHT22 - Humidity: "));
-  Serial.print(h);
-  Serial.print(F("%  Temperature: "));
-  Serial.print(t);
-  Serial.println(F(" °C"));
+  h = dht.readHumidity();
+  t = dht.readTemperature();
+  // if (isnan(h) || isnan(t)) {
+  //   Serial.println(F("Failed to read from DHT sensor!"));
+  //   return;
+  // }
+
+  // Serial.print(F("DHT22 - Humidity: "));
+  // Serial.print(h);
+  // Serial.print(F("%  Temperature: "));
+  // Serial.print(t);
+  // Serial.println(F(" °C"));
 }
 
 void readBH1750() {
-  float lux = lightMeter.readLightLevel();
-  Serial.print(F("BH1750 - Light: "));
-  Serial.print(lux);
-  Serial.println(F(" lx"));
+  lux = lightMeter.readLightLevel();
+  // Serial.print(F("BH1750 - Light: "));
+  // Serial.print(lux);
+  // Serial.println(F(" lx"));
 }
 
 void readUVSensor() {
-  Serial.print(F("UV Sensor - Visible: "));
-  Serial.print(uv.readVisible());
-  Serial.print(F(" IR: "));
-  Serial.print(uv.readIR());
-  Serial.print(F(" UV Index: "));
-  Serial.println(uv.readUV());
+  // Serial.print(F("UV Sensor - Visible: "));
+  // Serial.print(uv.readVisible());
+  // Serial.print(F(" IR: "));
+  // Serial.print(uv.readIR());
+  // Serial.print(F(" UV Index: "));
+  // Serial.println(uv.readUV());
 }
 
 void readLDRSensor() {
-  int val = analogRead(LDR_PIN);
-  Serial.print(F("LDR: "));
-  Serial.println(val);
+  val = analogRead(LDR_PIN);
+  // Serial.print(F("LDR: "));
+  // Serial.println(val);
 }
 
 void readSoilMoistureSensor() {
   sensor_analog = analogRead(sensor_pin);
   _moisture = (100 - ((sensor_analog / 4095.00) * 100));
-  Serial.print(F("Soil Moisture - Moisture: "));
-  Serial.print(_moisture);
-  Serial.println(F("%"));
+  // Serial.print(F("Soil Moisture - Moisture: "));
+  // Serial.print(_moisture);
+  // Serial.println(F("%"));
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println();
+
+  pinMode(P2Pin, OUTPUT);
+
+  mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
+  wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
+
+  // Initialize the sensors
+  dht.begin();
+  lightMeter.begin(BH1750::ONE_TIME_HIGH_RES_MODE);  // BH1750 sensor in high-resolution mode
+  uv.begin();
+
+  WiFi.onEvent(WiFiEvent);
+
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  mqttClient.onSubscribe(onMqttSubscribe);
+  mqttClient.onUnsubscribe(onMqttUnsubscribe);
+  //mqttClient.onPublish(onMqttPublish);
+  mqttClient.onMessage(onMqttMessage);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  // If your broker requires authentication (username and password), set them below
+  mqttClient.setCredentials(MQTT_USERNAME, MQTT_PASSWORD);
+  connectToWifi();
 }
 
 void loop() {
@@ -87,7 +364,7 @@ void loop() {
 
   // Read BH1750 sensor
   Wire.begin(BH1750_SDA, BH1750_SCL);  // Initialize I2C with BH1750 sensor pins
-  readBH1750();
+  //readBH1750();
   delay(1000);  // Delay between sensor readings
 
   // Read UV sensor
@@ -102,4 +379,44 @@ void loop() {
   // Read Soil Moisture sensor
   readSoilMoistureSensor();
   delay(1000);  // Delay between sensor readings
+  
+  //Check condition of temp on Fan
+  if(t>=35){
+    digitalWrite(P2Pin, HIGH);
+    //uint16_t packetIdPub6 = mqttClient.publish(MQTT_PUB_P2, 1, true, "on");
+  }
+  else{
+    digitalWrite(P2Pin, LOW);
+    //uint16_t packetIdPub6 = mqttClient.publish(MQTT_PUB_P2, 1, true, "off");
+  }
+
+
+  unsigned long currentMillis = millis();
+  // Every X number of seconds (interval = 5 seconds) 
+  // it publishes a new MQTT message
+  if (currentMillis - previousMillis >= interval) {
+    // Save the last time a new reading was published
+    previousMillis = currentMillis;
+    
+    //String testString = "Hello, world! #" + String(i);
+    //String testString = "Temp" + String(t) + " Hum" + String(h);
+    //Message to PUB
+    String Temp = String(t); //Temperature
+    String Hum = String(h); //Humidity
+    String lux1 = String(lux); //LightLevel
+    String val1 = String(val); //UVIndex
+    String _moisture1 = String(_moisture); //SoilMoisture
+
+    // Publish an MQTT message on topic test
+    //uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_TEST, 1, true, String(testString).c_str());
+    uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_TEMP, 1, true, String(Temp).c_str()); 
+    uint16_t packetIdPub2 = mqttClient.publish(MQTT_PUB_HUM, 1, true, String(Hum).c_str()); 
+    uint16_t packetIdPub3 = mqttClient.publish(MQTT_PUB_LR, 1, true, String(lux1).c_str());
+    uint16_t packetIdPub4 = mqttClient.publish(MQTT_PUB_UV, 1, true, String(val1).c_str()); 
+    uint16_t packetIdPub5 = mqttClient.publish(MQTT_PUB_MOI, 1, true, String(_moisture1).c_str());                            
+    //Serial.printf("Publishing on topic %s at QoS 1, packetId: %i", MQTT_PUB_TEST, packetIdPub1);
+    //Serial.printf(" Message: %.2f \n", testString);
+    i++;
+  }
+
 }
